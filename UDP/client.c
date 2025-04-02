00f528fc6fd93681a4b63c315183c9d5f3832ad4
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-04-02 16:13:16 +0530

    implemented hamming code encoding

--- UDP/client.c
+++ UDP/client.c
@@ -10,7 +10,9 @@
 
 const int PORT = 4444;
 const char *IP = "127.0.0.1";
-const int BUFFER_SIZE = 32;
+const int INPUT_BUFFER_SIZE = 32;
+const int OUTPUT_BUFFER_SIZE = INPUT_BUFFER_SIZE * 2;
+const char *TERMINATION_PROMPT = "END";
 
 
 void handle_error(const char *msg);
@@ -20,7 +22,7 @@ int main(int argc, char **argv) {
   int sock_fd;
   struct sockaddr_in sock_addr;
   socklen_t sock_size;
-  char * buffer;
+  char * input, *output;
 
   sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
   if (sock_fd == -1) {
@@ -33,22 +35,34 @@ int main(int argc, char **argv) {
 
   sock_size = sizeof(sock_addr);
 
-  buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+  input = (char *)malloc(sizeof(char) * INPUT_BUFFER_SIZE);
+  output = (char *)malloc(sizeof(char) * OUTPUT_BUFFER_SIZE);
 
-  strcpy(buffer, "Hello World!");
-  if (sendto(sock_fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&sock_addr, sock_size) == -1) {
-    handle_error("sendto");
+  while(1) {
+    bzero(input, INPUT_BUFFER_SIZE);
+    printf("Enter data: ");
+    fgets(input, INPUT_BUFFER_SIZE, stdin);
+    input[strlen(input)-1] = '\0';
+
+    if (sendto(sock_fd, input, strlen(input)+1, 0, (struct sockaddr *)&sock_addr, sock_size) == -1) {
+      handle_error("sendto");
+    }
+
+    if (!strcmp(input, TERMINATION_PROMPT)) {
+      break;
+    }
+
+    printf("[+]Data sent: %s\n", input);
+
+    bzero(output, OUTPUT_BUFFER_SIZE);
+    if (recvfrom(sock_fd, output, OUTPUT_BUFFER_SIZE, 0, (struct sockaddr *)&sock_addr, &sock_size) == -1) {
+      handle_error("recvfrom");
+    }
+    printf("[+}Data recv: %s\n", output);
   }
-  printf("[+]Data sent: %s\n", buffer);
 
-  bzero(buffer, BUFFER_SIZE);
-
-  if (recvfrom(sock_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&sock_addr, &sock_size) == -1) {
-    handle_error("recvfrom");
-  }
-  printf("[+}Data recv: %s\n", buffer);
-
-  free(buffer);
+  free(input);
+  free(output);
 
   if (close(sock_fd) == -1) {
     handle_error("close");
--- UDP/server.c
+++ UDP/server.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <unistd.h>
 #include <errno.h>
 #include <sys/socket.h>
@@ -10,17 +11,21 @@
 
 const int PORT = 4444;
 const char *IP = "127.0.0.1";
-const int BUFFER_SIZE = 32;
+const int INPUT_BUFFER_SIZE = 32;
+const int OUTPUT_BUFFER_SIZE = INPUT_BUFFER_SIZE * 2;
+const char *TERMINATION_PROMPT = "END";
 
 
 void handle_error(const char *msg);
+char *hammingCode(const char *inputData);
+char calculateParity(const char *data, int position);
 
 
 int main(int argc, char **argv) {
   int server_fd;
   struct sockaddr_in server_addr, client_addr;
   socklen_t server_size, client_size;
-  char *buffer;
+  char *input, *result;
 
   server_fd = socket(AF_INET, SOCK_DGRAM, 0);
   if (server_fd == -1) {
@@ -38,30 +43,40 @@ int main(int argc, char **argv) {
     handle_error("bind");
   }
 
-  printf("Server listening for connections...\n");
+  puts("Server listening for connections...");
 
-  buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+  input = (char *)malloc(sizeof(char) * INPUT_BUFFER_SIZE);
+  result = (char *)malloc(sizeof(char) * OUTPUT_BUFFER_SIZE);
 
-  if (recvfrom(server_fd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_size) == -1) {
-    handle_error("recvfrom");
+  while (1) {
+    bzero(input, INPUT_BUFFER_SIZE);
+    if (recvfrom(server_fd, input, INPUT_BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_size) == -1) {
+      handle_error("recvfrom");
+    }
+
+    if (!strcmp(input, TERMINATION_PROMPT)) {
+      break;
+    }
+
+    printf("[+]Data recv: %s\n", input);
+
+    bzero(result, OUTPUT_BUFFER_SIZE);
+    result = hammingCode(input);
+
+    if (sendto(server_fd, result, strlen(result)+1, 0, (struct sockaddr *)&client_addr, client_size) == -1) {
+      handle_error("sendto");
+    }
+    printf("[+]Data sent: %s\n", result);
   }
-  printf("[+]Data recv: %s\n", buffer);
 
-  bzero(buffer, BUFFER_SIZE);
-
-  strcpy(buffer, "Hello from server");
-  if (sendto(server_fd, buffer, strlen(buffer)+1, 0, (struct sockaddr *)&client_addr, client_size) == -1) {
-    handle_error("sendto");
-  }
-  printf("[+]Data sent: %s\n", buffer);
-
-  free(buffer);
+  free(input);
+  free(result);
 
   if (close(server_fd) == -1) {
     handle_error("close");
   }
 
-  printf("Terminating server...\n");
+  puts("Terminating server...");
 
   return EXIT_SUCCESS;
 }
@@ -69,4 +84,51 @@ int main(int argc, char **argv) {
 void handle_error(const char *msg) {
   fprintf(stderr, "%s failed with error: %d", msg, errno);
   exit(EXIT_FAILURE);
+}
+
+char *hammingCode(const char *inputData) {
+  char * result = (char *)malloc(sizeof(char) * OUTPUT_BUFFER_SIZE);
+
+  int inputDataSize = strlen(inputData), errorBitsNum = 1;
+
+  while (pow(2, errorBitsNum) < (inputDataSize + errorBitsNum + 1)) {
+    errorBitsNum++;
+  }
+
+  int i=strlen(inputData)-1, j=0, count = 0, size = inputDataSize + errorBitsNum;
+
+  while (j < size && i >= 0) {
+    if (pow(2, count)-1 == j) {
+      result[size - j - 1] = '0';
+      j++;
+      count++;
+      continue;
+    }
+    result[size - j - 1] = inputData[i--];
+    j++;
+  }
+
+  printf("Processing: %s\n", result);
+
+  for (int i=0; i<errorBitsNum; i++) {
+    int pos = pow(2, i);
+    result[size-pos] = calculateParity(result, pos);
+  }
+
+  printf("Final result: %s\n", result);
+
+  return result;
+}
+
+char calculateParity(const char *data, int position) {
+  int parity = 0, size = strlen(data), idx = size - position;
+
+  while (idx >= 0) {
+    for (int i=0; i<position; i++) {
+      parity ^= (data[idx--] - '0');
+    }
+    idx -= (position);
+  }
+
+  return parity + '0';
 }
