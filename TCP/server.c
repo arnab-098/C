bee1872a6098d846ef06ba47696954b2cc1f8515
arnab-098 <arnab.chattopadhyay777@gmail.com>
2025-06-16 11:51:35 +0530

    bit stuffing

--- TCP/server.c
+++ TCP/server.c
@@ -20,123 +20,118 @@ const int PORT = 4444;
 sem_t mutex;
 
 void signalHandler(int sigNum) {
-  if (sigNum == SIGINT) {
-    printf("\nShutting down server...\n");
-    exit(EXIT_SUCCESS);
-  }
+    if (sigNum == SIGINT) {
+        printf("\nShutting down server...\n");
+        exit(EXIT_SUCCESS);
+    }
 }
 
 void handleError(const char *error) {
-  fprintf(stderr, "%s failed with error: %d\n", error, errno);
-  exit(EXIT_FAILURE);
+    fprintf(stderr, "%s failed with error: %d\n", error, errno);
+    exit(EXIT_FAILURE);
 }
 
 void bitStuffing(char *inputData, char *result) {
-  int i, j, k, size, count;
+    int i, j, k, size, count;
 
-  i = 0;
-  j = 0;
-  size = strlen(inputData);
+    i = 0;
+    j = 0;
+    size = strlen(inputData);
 
-  while (i < size) {
-    if (inputData[i] == '1') {
-      count = 0;
-      result[j] = inputData[i];
-      for (k=i+1; inputData[k] == '0' && k < size && count < 5; k++) {
-        result[++j] = inputData[k];
-        count++;
-        if (count == 5) {
-          result[++j] = '0';
+    while (i < size) {
+        if (inputData[i] == '0') {
+            result[j++] = inputData[i++];
+            count = 0;
+        } else {
+            result[j++] = inputData[i++];
+            count++;
+            if (count == 5) {
+                result[j++] = '0';
+                count = 0;
+            }
         }
-        i = k;
-      }
-    } else {
-      result[j] = inputData[i];
     }
-    i++;
-    j++;
-  }
-  result[j] = '\0';
+    result[j] = '\0';
 }
 
 void *respond(void *args) {
-  int fd = *(int *)args;
+    int fd = *(int *)args;
 
-  char *inputData, *result;
+    char *inputData, *result;
 
-  inputData = (char *)malloc(sizeof(char) * BUFFER_SIZE);
-  result = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+    inputData = (char *)malloc(sizeof(char) * BUFFER_SIZE);
+    result = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 
-  while (1) {
-    read(fd, inputData, sizeof(inputData));
+    while (1) {
+        read(fd, inputData, sizeof(inputData));
 
-    if (strcmp(inputData, TERMINATION_PROMPT) == 0) {
-      break;
+        if (strcmp(inputData, TERMINATION_PROMPT) == 0) {
+            break;
+        }
+
+        bitStuffing(inputData, result);
+
+        write(fd, result, strlen(result));
+
+        sem_wait(&mutex);
+        printf("%s -> %s\n", inputData, result);
+        sem_post(&mutex);
+
+        sleep(1);
     }
 
-    bitStuffing(inputData, result);
+    free(inputData);
+    free(result);
 
-    write(fd, result, strlen(result));
-
-    sem_wait(&mutex);
-    printf("%s -> %s\n", inputData, result);
-    sem_post(&mutex);
-
-    sleep(1);
-  }
-
-  free(inputData);
-  free(result);
-
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 int main(int argc, char **argv) {
-  signal(SIGINT, signalHandler);
+    signal(SIGINT, signalHandler);
 
-  sem_init(&mutex, 0, 1);
+    sem_init(&mutex, 0, 1);
 
-  struct sockaddr_in s_addr, c_addr;
-  int sfd, cfd, c_addr_size, count;
+    struct sockaddr_in s_addr, c_addr;
+    int sfd, cfd, c_addr_size, count;
 
-  sfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sfd == -1) {
-    handleError("socket");
-  }
-
-  s_addr.sin_family = AF_INET;
-  s_addr.sin_port = htons(PORT);
-  s_addr.sin_addr.s_addr = inet_addr(IP);
-
-  if (bind(sfd, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
-    handleError("bind");
-  }
-
-  if (listen(sfd, LISTEN_BACKLOG) == -1) {
-    handleError("listen");
-  }
-
-  printf("Server is waiting for connection...\n");
-
-  while (1) {
-    c_addr_size = sizeof(cfd);
-    cfd = accept(sfd, (struct sockaddr *) &c_addr, &c_addr_size);
-    if (cfd == -1) {
-      handleError("accept");
+    sfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sfd == -1) {
+        handleError("socket");
     }
 
-  	pthread_t thread;
+    s_addr.sin_family = AF_INET;
+    s_addr.sin_port = htons(PORT);
+    s_addr.sin_addr.s_addr = inet_addr(IP);
 
-    if (pthread_create(&thread, NULL, &respond, (void *)&cfd) != 0) {
-      handleError("pthread_create");
+    if (bind(sfd, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+        handleError("bind");
     }
-  }
 
-  if (close(sfd) == -1) {
-    handleError("close");
-  }
+    if (listen(sfd, LISTEN_BACKLOG) == -1) {
+        handleError("listen");
+    }
 
-  sem_destroy(&mutex);
+    printf("Server is waiting for connection...\n");
 
-  return EXIT_SUCCESS;
+    while (1) {
+        c_addr_size = sizeof(cfd);
+        cfd = accept(sfd, (struct sockaddr *) &c_addr, &c_addr_size);
+        if (cfd == -1) {
+            handleError("accept");
+        }
+
+        pthread_t thread;
+
+        if (pthread_create(&thread, NULL, &respond, (void *)&cfd) != 0) {
+            handleError("pthread_create");
+        }
+    }
+
+    if (close(sfd) == -1) {
+        handleError("close");
+    }
+
+    sem_destroy(&mutex);
+
+    return EXIT_SUCCESS;
 }
